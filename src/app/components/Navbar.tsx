88d405a248753f519ae374da8fb5b143086bcbad
hensuchoi <hensu.choi@gmail.com>
2025-03-08 01:09:52 -0500

    bugs fixed

--- src/app/components/Navbar.tsx
+++ src/app/components/Navbar.tsx
@@ -1,5 +1,23 @@
-<Link href="/" className="flex items-center space-x-2">
-  <span className="brand-text text-2xl brand-gradient">
-    Chiquinegos
-  </span>
-</Link> 
+'use client';
+
+import Link from 'next/link';
+
+const Navbar = () => {
+  return (
+    <nav className="bg-white shadow-sm">
+      <div className="max-w-7xl mx-auto px-4 sm:px-6 lg:px-8">
+        <div className="flex justify-between h-16">
+          <div className="flex">
+            <Link href="/" className="flex items-center space-x-2">
+              <span className="brand-text text-2xl brand-gradient">
+                Chiquinegos
+              </span>
+            </Link>
+          </div>
+        </div>
+      </div>
+    </nav>
+  );
+};
+
+export default Navbar; 
